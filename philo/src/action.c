be44cdf1de62d005ee58c55fcffed9570be9aee0
josuhee <suheeqwe@gmail.com>
2022-01-01 00:56:18 -0800

    philo's action

--- philo/src/action.c
+++ philo/src/action.c
@@ -6,7 +6,7 @@
 /*   By: sujo <sujo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/30 07:42:23 by sujo              #+#    #+#             */
-/*   Updated: 2021/12/30 08:56:50 by sujo             ###   ########.fr       */
+/*   Updated: 2022/01/01 00:38:32 by sujo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,19 @@ void fork(t_philo *philo)
 	if (philo->info->is_die || philo->info->is_must_eat)
 		return ;
 	pthread_mutex_lock(&philo->info->forks[philo->left]);
-	// print get fork
+	philo_status_prnt(philo, FORK);
 	pthread_mutex_lock(&philo->info->forks[philo->right]);
-	// print get fork
+	philo_status_prnt(philo, FORK);
 }
 
 void eat(t_philo *philo)
 {
 	if (philo->info->is_die || philo->info->is_must_eat)
 		return ;
+	philo_status_prnt(philo, EAT);
+	vsleep(philo->info->eat);
+	philo->eat_cnt = philo->eat_cnt + 1;
+	philo->last_eat_time = now_time();
 }
 
 void unfork(t_philo *philo)
@@ -34,4 +38,20 @@ void unfork(t_philo *philo)
 		return ;
 	pthread_mutex_unlock(&philo->info->forks[philo->left]);
 	pthread_mutex_unlock(&philo->info->forks[philo->right]);
+}
+
+void go_sleep(t_philo *philo)
+{
+	if (philo->info->is_die || philo->info->is_must_eat)
+		return ;
+	philo_status_prnt(philo, SLEEP);
+	//philo->last_eat_time = now_time();
+	vsleep(philo->info->sleep);
+}
+
+void think(t_philo *philo)
+{
+	if (philo->info->is_die || philo->info->is_must_eat)
+		return ;
+	philo_status_prnt(philo, THINK);
 }
